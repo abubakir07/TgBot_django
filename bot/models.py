402403f6e0244944baefea68f43f999f7247771f
abubakir07 <amahamatjanov0071225@gmail.com>
2022-12-29 15:24:52 +0600

    add User and Post

---
+++ bot/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class User(models.Model):
+    external_id = models.PositiveBigIntegerField()
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    username = models.CharField(max_length=255)
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
